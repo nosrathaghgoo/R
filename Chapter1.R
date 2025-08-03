304c1a7c1f39bab21509558882c2d0bea95ef582
Nosratollah Haghgoo <nosyc@yahoo.com>
2025-08-03 09:15:45 +0330

    Chapter1 (R for Data Science_ Import, Tidy, Transform, Visualize, and Model Data-O'Reilly Media (2023))

---
+++ Chapter1.R
@@ -0,0 +1,17 @@
+install.packages("tidyverse")
+library(tidyverse)
+library(palmerpenguins)
+library(ggthemes)
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point(aes(color = species, shape = species)) +
+  geom_smooth(method = "lm") +
+  labs(
+    title = "Body mass and flipper length",
+    subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+    x = "Flipper length (mm)", y = "Body mass (g)",
+    color = "Species", shape = "Species"
+  ) +
+  scale_color_colorblind()
